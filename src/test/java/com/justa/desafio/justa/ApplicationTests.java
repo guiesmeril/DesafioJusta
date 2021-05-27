448887579d062dcfb3fff560dbbe7fb93ef997c7
guiesmeril <guiesmeril@hotmail.com>
2021-05-27 00:41:10 -0300

    Commit com o Restante do projeto

---
+++ src/test/java/com/justa/desafio/justa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justa.desafio.justa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
